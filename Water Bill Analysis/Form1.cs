12fae2715c9450aa0ec5cd2d979ea5f784455cfd
alicia65 <alicia65>
2020-03-07 23:43:35 -0600

    add new methods getBillAverage and getBillTotals.

--- Water Bill Analysis/Form1.cs
+++ Water Bill Analysis/Form1.cs
@@ -24,16 +24,54 @@ namespace Water_Bill_Analysis
 
         private void btnCalculate_Click_1(object sender, EventArgs e)
         {
-            string[] quarters = {"first quarter", "second quarter", "third quarter", "fourth quarter" };
-            foreach(string i in quarters) 
+            
+            double[] quarters = new double[4];
+            foreach(double i in quarters) 
             
             {
-                lblFirstQuarter(Convert.ToInt32(i));//change data type from text to integer using convert method
-                lblSecondQuarter(Convert.ToInt32(i));
-                lblThirdQuarter(Convert.ToInt32(i));
-                lblFourthQuarter(Convert.ToInt32(i));
+                lblFirstQuarter(Convert.ToDouble(i));//change data type from text to integer using convert method
+                MessageBox.Show(txtFirstQuarter);
+
+                lblSecondQuarter(Convert.ToDouble(i));
+                MessageBox.Show(txtSecondQuarter);
+                
+                lblThirdQuarter(Convert.ToDouble(i));
+                MessageBox.Show(txtThirdQuarter);
+                
+                lblFourthQuarter(Convert.ToDouble(i));
+                MessageBox.Show(txtFourthQuarter);
+                                       
+                   
             }
 
+            getBillAverage();//Call method to get average
+            getBillTotals();// Call method to get totals
+
         }
+
+        public void getBillTotals(double t) 
+        {
+            double[] quarters = new double[4];
+            t = 0;
+            for (double k = 0; k< quarters.Length; k++)
+                t += quarters[k];
+            txtTotal.Text = t;
+        }
+
+        public void getBillAverage(double average) 
+        {
+            double[] quarters = new double[4];
+            average = 0;
+            if (average > 0)
+            {
+                average = quarters[0] + quarters[1] + quarters[2] + quarters[3] / quarters.Length;
+                txtAverage.Text = TryParse.ToDouble(average);
+            }
+            else 
+            {
+                MessageBox.Show("Try again", "error");
+            }
+        }
+        
     }
 }
