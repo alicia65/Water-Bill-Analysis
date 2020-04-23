96779989d93af2c8de9249e78071ac209e80358c
alicia65 <alicia65>
2020-04-23 01:07:38 -0500

    add new class to app

---
+++ Water Bill Analysis/Bill.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Water_Bill_Analysis
+{
+    class Bill
+    {
+        //Create constructor
+        public Bill(double firstQuarter, double secondQuarter, double thirdQuarter, double fourthQuarter)
+        {
+            FirstQuarter = firstQuarter;
+            SecondQuarter = secondQuarter;
+            ThirdQuarter = thirdQuarter;
+            FourthQuarter = fourthQuarter;
+        }
+
+        // Auto Preperties
+        public double FirstQuarter { get; set; }
+        public double SecondQuarter { get; set; }
+        public double ThirdQuarter { get; set; }
+        public double FourthQuarter { get; set; }
+
+
+    }
+}
+
+        
+    
+
--- Water Bill Analysis/Form1.cs
+++ Water Bill Analysis/Form1.cs
@@ -12,21 +12,22 @@ namespace Water_Bill_Analysis
 {
     public partial class Form1 : Form
     {
+       
         double[] quarterLists = new double[4];// Create new arrary to store data
         //initialize global variables and assign zero values to them
-        double firstQuarter = 0;
-        double secondQuarter = 0;
-        double thirdQuarter = 0;
-        double fourthQuarter = 0;
-        double billTotal = 0;
-        double billAverage = 0;
+        double firstQuarter = 0.0;
+        double secondQuarter = 0.0;
+        double thirdQuarter = 0.0;
+        double fourthQuarter = 0.0;
+        double billTotal = 0.0;
+        double billAverage = 0.0;
         public Form1()
         {
             InitializeComponent();
-            //getTotals(billTotal);
-            //getAverage(billAverage);
 
+            
         }
+
 
         private void btnCalculate_Click_1(object sender, EventArgs e)
         {
@@ -87,11 +88,7 @@ namespace Water_Bill_Analysis
             txtAverage.Text = "$" + average; // Display average  bill on text box 
 
         }
-
-
-
-        
-
+                
         private void Form1_Load(object sender, EventArgs e)
         {
 
